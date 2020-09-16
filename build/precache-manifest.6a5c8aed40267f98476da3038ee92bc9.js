501242df82df77d618468d6428c7e96b28be3b5e
shubhdhawan <46443270+shubhdhawan@users.noreply.github.com>
2020-09-16 19:17:52 +0530

    Add files via upload

---
+++ build/precache-manifest.6a5c8aed40267f98476da3038ee92bc9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0036564582ad9ac55c56fff2811894f4",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/index.html"
+  },
+  {
+    "revision": "0d9cf3e00645c42c977b",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/css/main.5125e6d1.chunk.css"
+  },
+  {
+    "revision": "20bd7a6988e6aa3d2404",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/js/2.65ba865c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/js/2.65ba865c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9cf3e00645c42c977b",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/js/main.e6fd4468.chunk.js"
+  },
+  {
+    "revision": "31c62e6a7a454f7b55c8",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/js/runtime-main.64ff003b.js"
+  },
+  {
+    "revision": "eb68ce55380a2b4f127547818823bf0b",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Bitmap.eb68ce55.png"
+  },
+  {
+    "revision": "c2372cc7850e5aafed2102e04ef30f27",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Cart.c2372cc7.svg"
+  },
+  {
+    "revision": "5d694365ef6a5c64b193c5ccbc2fe286",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Combined Shape.5d694365.svg"
+  },
+  {
+    "revision": "beacfdc122681fabe762caadc3a6ba8e",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Facebook.beacfdc1.svg"
+  },
+  {
+    "revision": "12f8cf27c80059398edd3f52459251f1",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Linkedin.12f8cf27.svg"
+  },
+  {
+    "revision": "d575357d5df826d323c2c5c72e81308f",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Notification.d575357d.svg"
+  },
+  {
+    "revision": "4a5654c8e31ba04874326989b3f91f6a",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Orion_archive-box.4a5654c8.svg"
+  },
+  {
+    "revision": "4a7c61abdf05b64630c8e1943345c544",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Orion_secure-payment (1).4a7c61ab.svg"
+  },
+  {
+    "revision": "3f1e02c7776e5e619dddfa2bdee5c1cd",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Orion_truck.3f1e02c7.svg"
+  },
+  {
+    "revision": "f3a10c42fc8cd8073d0629a0290b20b7",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Profile.f3a10c42.svg"
+  },
+  {
+    "revision": "724706a41b50e7f0b8e28e7362fe0c60",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Reserach.724706a4.svg"
+  },
+  {
+    "revision": "20b4003eb64d0bb39710c871a3941cc0",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Star.20b4003e.svg"
+  },
+  {
+    "revision": "65715d46e19ce9c55ddc896943fde6fc",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Starnotmarked.65715d46.svg"
+  },
+  {
+    "revision": "f0e4b8401abf91c933d55d0923f12b7f",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/Twitter.f0e4b840.svg"
+  },
+  {
+    "revision": "e61f0e84ef35ab29a2bfb27229b942cd",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/book1.e61f0e84.png"
+  },
+  {
+    "revision": "9a97c2dff8b503d4d3424b816d1423e8",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/image1.9a97c2df.png"
+  },
+  {
+    "revision": "e67b6f3fa973b732d829e6df5928df0a",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/image2.e67b6f3f.png"
+  },
+  {
+    "revision": "12a3a8c97fe982e5ec35be32e52399eb",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/image3.12a3a8c9.png"
+  },
+  {
+    "revision": "708a1249f787b9ea5bd58c5bca7ea4df",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/logo_Taxmann--Tax-&-CL-of-India-for-Web_Sept18 (1).708a1249.svg"
+  },
+  {
+    "revision": "1268a4b2e5deeda6264c4ac536d4dcea",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/logo_Taxmann--Tax-&-CL-of-India-for-Web_Sept18.1268a4b2.svg"
+  },
+  {
+    "revision": "5c440e01149c06baef1744b4e0eef420",
+    "url": "/Users/asus/Desktop/projects/Hiring%20Task/build/static/media/vol2L.5c440e01.png"
+  }
+]);
